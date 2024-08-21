27e57effbd842bb5b18efdc7c98a5757116048f9
hykeegj <hykeegj@gmail.com>
2024-08-21 21:57:07 +0900

    3.13

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,25 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main
+      className="flex h-screen items-center justify-center bg-gray-100 p-5
+        sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100
+        xl:bg-orange-100 2xl:bg-purple-100"
+    >
+      <div
+        className="flex w-full max-w-screen-sm flex-col gap-4 rounded-3xl
+          bg-white p-5 shadow-lg"
+      >
+        <div className="group flex flex-col">
+          <input
+            className="w-full bg-gray-100"
+            placeholder="Write you email"
+          />
+          <span className="hidden group-focus-within:block">
+            Make sure it is a valid email...
+          </span>
+          <button>Submit</button>
+        </div>
+      </div>
+    </main>
+  );
 }
