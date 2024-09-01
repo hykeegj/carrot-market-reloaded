9b613770b7e16ed3256d384a03a6df224c1af59c
hykeegj <hykeegj@gmail.com>
2024-09-01 21:51:13 +0900

    #6.0 Introduction to Zod (11:03)

---
+++ components/form-btn.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useFormStatus } from "react-dom";
+
+interface FormButtonProps {
+  text: string;
+}
+
+export default function FormButton({
+  text,
+}: FormButtonProps) {
+  const { pending } = useFormStatus();
+  return (
+    <button
+      disabled={pending}
+      className="primary-btn h-10 disabled:cursor-not-allowed
+        disabled:bg-neutral-400 disabled:text-neutral-300"
+    >
+      {pending ? "로딩 중" : text}
+    </button>
+  );
+}
